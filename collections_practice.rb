38c5fc5d3deb0d2d4655719f27e2f2e5b6e2f50d
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-12 23:57:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,9 +19,8 @@ def reverse_array (array)
 end
 
 def kesha_maker (array)
-  array.each do |word|
-    return word.delete(word[2])
+  return array.each do |word|
+    word.delete(word[2])
     #new_word.insert(2,"$")
   end
-  array
 end
